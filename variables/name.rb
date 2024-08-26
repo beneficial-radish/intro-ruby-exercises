43602aed7f1db443cb0bb98841a5cd9c41d01ee7
beneficial-radish <144536648+beneficial-radish@users.noreply.github.com>
2024-08-26 16:00:06 -0500

    Modify name.rb to get first and last name

--- variables/name.rb
+++ variables/name.rb
@@ -1,4 +1,6 @@
-print "What's your name? "
-name = gets.chomp
-puts "Hello, #{name}!"
-10.times {puts name} 
+print "What's your first name? "
+first_name = gets.chomp
+print "What's your last name? "
+last_name = gets.chomp
+puts "Hello, #{first_name} #{last_name}!"
+10.times {puts first_name + " " + last_name} 
