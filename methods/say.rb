1f98be9d6efb905a265afc3c50503091b44e4b58
beneficial-radish <144536648+beneficial-radish@users.noreply.github.com>
2024-08-26 17:39:14 -0500

    Move Methods exercises to correct file

---
+++ methods/say.rb
@@ -0,0 +1,8 @@
+def say (words='hello')
+  puts words
+end
+
+say("hello")
+say("In Ruby, a function is always a method because everything is an object.")
+say("So, in Ruby, calling a method is also known as \'method invocation\'")
+say("parentheses are optional when passing arguments. So, it can be difficult to differentiate between local variables and method names.")
