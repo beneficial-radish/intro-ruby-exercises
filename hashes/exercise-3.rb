36443a2380704d32b0561e6328bce444cd1261f6
beneficial-radish <144536648+beneficial-radish@users.noreply.github.com>
2024-08-28 12:45:00 -0500

    Complete exercise 3 in Hashes

---
+++ hashes/exercise-3.rb
@@ -0,0 +1,10 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+
+family.each{|key, value| puts key}
+family.each{|key, value| puts value}
+
+family.each{|key, value| print value; print key}
